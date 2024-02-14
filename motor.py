55ebd134579533fd34ca80e9c1ff592293d32d45
JIYUAN GUO <46417086+JiDian93@users.noreply.github.com>
2024-02-14 19:44:55 +0000

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,21 +1,26 @@
 import RPi.GPIO as GPIO  # Imports the standard Raspberry Pi GPIO library
-from time import sleep   # Imports sleep (aka wait or pause) into the program
-GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
+from time import sleep   
 
+class DoorController:
+    def __init__(self, pin, frequency):
+        self.pin = 11
+        self.frequency = 50
+        GPIO.setmode(GPIO.BOARD)  # Sets the pin numbering system to use the physical layout
+        GPIO.setup(self.pin, GPIO.OUT)  # Sets up pin to an output
+        self.pwm = GPIO.PWM(self.pin, self.frequency)  # Sets up pin as a PWM pin
 
-GPIO.setup(11,GPIO.OUT)  # Sets up pin 11 to an output 
-pwm = GPIO.PWM(11, 50)     # Sets up pin 11 as a PWM pin
+    def door_open(self):  # turn 90 degree
+        self.pwm.start(5)
+        sleep(0.07)
+        self.pwm.stop()
 
-def door_open():    #turn 90 degree
-    pwm.start(5)               
-    sleep(0.07)    
-    pwm.stop()     
-	
+    def door_close(self):  # turn -90 degree
+        self.pwm.start(9)
+        sleep(0.07)
+        self.pwm.stop()
 
-def door_close():   #turn -90 degree
-    pwm.start(9)               
-    sleep(0.07)    
-    pwm.stop()  
+    def cleanup(self):
+        GPIO.cleanup()  # Cleans up the GPIO pins
 
 #test
 #door_open()
