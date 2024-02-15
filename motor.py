d70b0711ecabe8f18fa4963b5d45253cea1882f7
JIYUAN GUO <46417086+JiDian93@users.noreply.github.com>
2024-02-15 02:08:48 +0000

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,30 +1,27 @@
 import RPi.GPIO as GPIO  # Imports the standard Raspberry Pi GPIO library
 from time import sleep   
+from gate_indicator import GateIndicator
 
-class DoorController:
-    def __init__(self, pin, frequency):
+
+
+class DoorIndicator(GateIndicator):
+    def __init__(self):
         self.pin = 11
         self.frequency = 50
-        GPIO.setmode(GPIO.BOARD)  # Sets the pin numbering system to use the physical layout
-        GPIO.setup(self.pin, GPIO.OUT)  # Sets up pin to an output
-        self.pwm = GPIO.PWM(self.pin, self.frequency)  # Sets up pin as a PWM pin
-
-    def door_open(self):  # turn 90 degree
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pin, self.frequency)
+    
+    def on_gate_open(self):
         self.pwm.start(5)
         sleep(0.07)
         self.pwm.stop()
 
-    def door_close(self):  # turn -90 degree
+    def on_gate_close(self):
         self.pwm.start(9)
         sleep(0.07)
         self.pwm.stop()
 
     def cleanup(self):
-        GPIO.cleanup()  # Cleans up the GPIO pins
+        GPIO.cleanup()
 
-#test
-#door_open()
-#door_close()
-
- 
-#GPIO.cleanup() 
