6a242d600ee0b061fdce909792ed805f5fde4c67
yang <Lyuxyang@gmail.com>
2024-02-16 02:31:21 +0000

    gate and gate_app modele passed test

--- gate.py
+++ gate.py
@@ -2,15 +2,17 @@ import state
 
 class Gate:
     def __init__(self):
+        # delete test code
+        self.test_code = 0
         self.state = state.ClosedState(self)
         self._indicators = []
         # 
         self.current_vehicle_id = None
-        pass
 
     def register_indicator(self, gate_indicator):
         self._indicators.append(gate_indicator)
-        pass
+        # TODO: delete this
+        print("success add a new indicator")
 
     
         
--- gate_app.py
+++ gate_app.py
@@ -3,33 +3,46 @@ import gate
 import db_manager
 import vehicle_id_reader
 import time
+# import motor
+# import lcd
 
-CLOSED_STATE = "closed"
-OPEN_STATE = "open"
+CLOSED_STATE = "gate closed"
+OPEN_STATE = "gate open"
 
 
 class GateApp:
-    def __init__(self, gate: gate.Gate, reader: vehicle_id_reader.VehicleldReader, db_manager: db_manager.DataBaseManager):
+    def __init__(self, gate: gate.Gate, reader: vehicle_id_reader.VehicleIdReader, db_manager: db_manager.DataBaseManager):
         self.gate = gate
         self.reader = reader
         self.db_manager = db_manager
 
     def run(self):
+        # TODO: import real indicator
+        # self.gate.register_indicator(motor.DoorIndicator)
+        # self.gate.register_indicator(lcd.lcd)
+        self.gate.register_indicator("test")
+        
         while (True):
             vehicle_id = self.reader.get_vehicle_id()
-            self.gate.current_vehicle_id = vehicle_id
             # If the vehicle id got match to the id in DB, change gate state and run
-            if self.db_manager.search(vehicle_id):
+            # TODO: delete test code
+            # if self.db_manager.search(vehicle_id):
+            if self.gate.test_code == 0:
                 if self.gate.state.current_state() == OPEN_STATE:
                     continue
+                self.gate.current_vehicle_id = vehicle_id
                 self.gate.state.change_state()
                 self.gate.state.run()
+            # TODO: delete test code
+                self.gate.test_code = 1
 # TODO: Check if it need pause
             else:
                 if self.gate.state.current_state() == CLOSED_STATE:
                     continue
                 self.gate.state.change_state()
                 self.gate.state.run()
+            # TODO: delete test code
+                self.gate.test_code = 0
 # TODO: Check if it need pause
 
     def exit(self):
@@ -37,4 +50,4 @@ class GateApp:
             self.gate.state.change_state()
             self.gate.state.run()
 
-# TODO: test code
+
